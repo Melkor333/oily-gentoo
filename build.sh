f25115e2bf99f1e079d6137dbb72040a4ec444c1
Samuel Hierholzer (Adfinis AG) <samuel.hierholzer@adfinis.com>
2024-06-25 14:01:29 +0200

    use build file and fix in emerge command

---
+++ build.sh
@@ -0,0 +1,30 @@
+STAGE3_DIR=/var/stage3
+
+chroot() {
+  sudo mount --rbind /dev $STAGE3_DIR/dev || echo already mounted
+  sudo mount --make-rslave $STAGE3_DIR/dev || echo already mounted
+  sudo mount -t proc /proc $STAGE3_DIR/proc || echo already mounted
+  sudo mount --rbind /sys $STAGE3_DIR/sys || echo already mounted
+  sudo mount --make-rslave $STAGE3_DIR/sys || echo already mounted
+  sudo mount --bind /run $STAGE3_DIR/run || echo already mounted
+  sudo chroot /var/stage3 /bin/bash
+}
+
+rootfs() {
+  sudo ./mkcascade
+}
+
+remove_mounts() {
+  sudo umount -lf /var/stage3/dev
+  sudo umount -lf /var/stage3/proc
+  sudo umount -lf /var/stage3/sys
+  sudo umount -lf /var/stage3/cascade
+}
+
+clean() {
+  remove_mounts
+  sudo rm -rf /var/stage3
+  sudo rm -rf target/rootfs
+}
+
+"$@"
